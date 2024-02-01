45a1465f6e170baf39024575a349ca73904faa39
Kim Da Eun <midekuna@gmail.com>
2024-02-01 16:15:00 +0900

    docs: 프레임워크 없는 프론트엔드 개발 4장 (3)
    - HTML 코드에서 모든 코드를 템플릿화 하고, 사용자 정의 컴포넌트의 이름으로 마크업 변경
    - todomvc-list와 todomvc-app 컴포넌트 구현
    - troubleshooting: 콜백함수 안에서의 this
    - 렌더링 함수 방식과 컴포넌트 방식의 장단점

---
+++ 프레임워크-없는-프론트엔드-개발/src/components/App.js
@@ -0,0 +1,59 @@
+import { EVENTS } from './List.js';
+
+export default class App extends HTMLElement {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      filter: 'All',
+    };
+
+    this.template = document.getElementById('todo-app');
+  }
+
+  deleteItem(index) {
+    this.state.todos.splice(index, 1);
+    this.syncAttributes();
+  }
+
+  addItem(text) {
+    this.state.todos.push({
+      text,
+      completed: false,
+    });
+    this.syncAttributes();
+  }
+
+  /**
+   * 자식 컴포넌트의 상태와 부모 컴포넌트의 상태를 동기화
+   */
+  syncAttributes() {
+    this.list.todos = this.state.todos;
+    this.footer.todos = this.state.todos;
+    this.footer.filter = this.state.filter;
+  }
+
+  connectedCallback() {
+    window.requestAnimationFrame(() => {
+      const content = this.template.content.firstElementChild.cloneNode(true);
+
+      this.appendChild(content);
+
+      this.querySelector('.new-todo').addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+          this.addItem(e.target.value);
+          e.target.value = '';
+        }
+      });
+
+      this.footer = this.querySelector('todomvc-footer');
+
+      this.list = this.querySelector('todomvc-list');
+      this.list.addEventListener(EVENTS.DELETE_ITEM, (e) => {
+        this.deleteItem(e.detail.index);
+      });
+
+      this.syncAttributes();
+    });
+  }
+}
--- 프레임워크-없는-프론트엔드-개발/src/index.js
+++ 프레임워크-없는-프론트엔드-개발/src/index.js
@@ -1,43 +1,7 @@
-import todosView from './view/todos.js';
-import counterView from './view/counter.js';
-import filtersView from './view/filters.js';
-import appView from './view/app.js';
-import applyDiff from './applyDiff.js';
+import App from './components/App.js';
+import Footer from './components/Footer.js';
+import List from './components/List.js';
 
-import registry from './registry.js';
-
-registry.add('app', appView);
-registry.add('todos', todosView);
-registry.add('counter', counterView);
-registry.add('filters', filtersView);
-
-const state = {
-  todos: [],
-  currentFilter: 'All',
-};
-
-const events = {
-  deleteItem: (index) => {
-    state.todos.splice(index, 1);
-    render();
-  },
-  addItem: (text) => {
-    state.todos.push({
-      text,
-      completed: false,
-    });
-    render();
-  },
-};
-
-const render = () => {
-  window.requestAnimationFrame(() => {
-    const main = document.querySelector('#root');
-
-    const newMain = registry.renderRoot(main, state, events);
-
-    applyDiff(document.body, main, newMain);
-  });
-};
-
-render();
+window.customElements.define('todomvc-app', App);
+window.customElements.define('todomvc-list', List);
+window.customElements.define('todomvc-footer', Footer);
